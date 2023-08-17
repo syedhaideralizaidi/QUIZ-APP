5b2642c919d0ec58ebba55b1f860b93dd7fc5465
syedhaideralizaidi <haider.zaidiy@gmail.com>
2023-08-17 11:59:57 +0500

    Quiz cant be saved until questions are valid

--- quizapp/base/formset_view.py
+++ quizapp/base/formset_view.py
@@ -1,6 +1,6 @@
 from django.db import transaction , IntegrityError
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect , render
 from django.urls import reverse
 from .models import User, QuizAssignment, ClassroomStudentEnrolled
 from .forms import QuizForm, QuizFormSet
@@ -32,31 +32,27 @@ class QuizCreateView(CreateView):
         try:
             with transaction.atomic():
                 req = self.request.POST
-                quiz = form.save(commit=False)
-                quiz.teacher = self.request.user
-                quiz.save()
                 question_formset = context["question_formset"]
                 students = User.students.all()
                 number_count = 0
                 if question_formset.is_valid():
-                    self.object = form.save()
-                    question_formset.instance = self.object
-                    count_questions = req['quiz_question-TOTAL_FORMS']
+                    count_questions = req[ 'quiz_question-TOTAL_FORMS' ]
                     count_questions = int(count_questions)
                     print(number_count)
-                    # count_questions = int(count_questions)
-                    for question in range(0, count_questions):
+                    for question in range(0 , count_questions) :
                         value = f'quiz_question-{question}-score'
-                        number_count += int(req[value])
+                        number_count += int(req[ value ])
 
                     if number_count != int(req['total_score']) or req['required_score'] > req['total_score']:
                         return self.form_invalid(form)
-                    else:
+                    else :
                         pass
+                    quiz = form.save(commit = False)
+                    quiz.teacher = self.request.user
+                    quiz.save()
+                    self.object = form.save()
+                    question_formset.instance = self.object
                     question_formset.save()
-
-                # else:
-                #     return HttpResponse("Invalid form Submission, Also fill the questions with the Quiz!. Fill it again")
                 else:
                     return self.form_invalid(form)
                 classroom = quiz.classroom
@@ -74,10 +70,10 @@ class QuizCreateView(CreateView):
     def form_invalid(self, form):
         context = self.get_context_data()
         question_formset = context["question_formset"]
-        return self.render_to_response(
-            self.get_context_data(form=form, question_formset=question_formset)
-        )
-
+        # return self.render_to_response(
+        #     self.get_context_data(form=form, question_formset=question_formset)
+        # )
+        return render(self.request, 'templates/base/invalid.html', context = {'title': 'Invalid Form Submission.', 'message': 'Check if your required score is less than total score or your questions score total is equal to total score of Quiz.'} )
     def get_success_url(self):
         return redirect("/dashboard_teacher")
 
